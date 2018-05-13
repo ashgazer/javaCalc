dc7df4a5954f717866fc44d35638d548ae887042
Ashik Pirmohamed <ashik.pirmohamed@wahanda.com>
2018-05-13 17:49:10 +0100

    moved case statement to it's own class

--- src/com/ashik/calcengine/Main.java
+++ src/com/ashik/calcengine/Main.java
@@ -5,55 +5,38 @@ public class Main {
 
     public static void main(String[] args) {
 
+//
+//        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d };
+//        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d };
+//        char[] opCodes = {'d', 'a', 's', 'm' };
+//        double[] results = new double[opCodes.length];
 
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d };
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d };
-        char[] opCodes = {'d', 'a', 's', 'm' };
-        double[] results = new double[opCodes.length];
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 50.0d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 's');
+        equations[3] = create(11.0d, 3.0d, 'm');
 
-        for (int i = 0; i < opCodes.length; i++) {
-            switch(opCodes[i]) {
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case 's':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case 'd':
-                    results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
-                    break;
-                case 'm':
-                    results[i] = leftVals[i] * rightVals[i];
-                    break;
-                default:
-                {
-                System.out.println("bad operator");
-                results[i] = 0.00d;
-                break;
-            }
 
-            }
-//            if (opCodes[i] == 'a')
-//                results[i] = leftVals[i] + rightVals[i];
-//            else if (opCodes[i] == 's')
-//                results[i] = leftVals[i] - rightVals[i];
-//            else if (opCodes[i] == 'd')
-//                results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
-//            else if (opCodes[i] == 'm')
-//                results[i] = leftVals[i] * rightVals[i];
-//            else {
-//                System.out.println("bad operator");
-//                results[i] = 0.00d;
-//            }
+
+        for( MathEquation equation : equations) {
+            equation.execute();
+            System.out.println(equation.result);
 
         }
-
-        for ( double theresult : results)
-                    System.out.println(theresult);
-
-
 
 
 
     }
+
+   public static MathEquation create(double leftVal, double rightVal, char opCode) {
+
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+
+        return equation;
+   }
+
 }
